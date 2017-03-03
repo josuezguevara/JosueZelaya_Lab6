6341cdcad260cc1817fbdfa55b838482b75b1869
Josue Zelaya <josuezelaya99@hotmail.es>
2017-03-03 14:29:02 -0600

    Clase Empleado

---
+++ JosueZelaya_Lab6/src/josuezelaya_lab6/Empleado.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package josuezelaya_lab6;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import javax.swing.tree.DefaultTreeModel;
+
+/**
+ *
+ * @author usuario
+ */
+public class Empleado extends Persona{
+    private String seccion_trabajo;
+    private String hora_entrada;
+    private String hora_salida;
+    private long sueldo;
+    private String estado;
+
+    public Empleado() {
+        super();
+    }
+
+    public Empleado(String seccion_trabajo, String hora_entrada, String hora_salida, long sueldo, String estado, int edad, long id, String nacionalidad, String lugar_nacimiento, String nombre, DefaultTreeModel arbol_genealogico, Color color_piel, ArrayList<Familiares> familiares) {
+        super(edad, id, nacionalidad, lugar_nacimiento, nombre, arbol_genealogico, color_piel, familiares);
+        this.seccion_trabajo = seccion_trabajo;
+        this.hora_entrada = hora_entrada;
+        this.hora_salida = hora_salida;
+        this.sueldo = sueldo;
+        this.estado = estado;
+    }
+
+    public String getSeccion_trabajo() {
+        return seccion_trabajo;
+    }
+
+    public void setSeccion_trabajo(String seccion_trabajo) {
+        this.seccion_trabajo = seccion_trabajo;
+    }
+
+    public String getHora_entrada() {
+        return hora_entrada;
+    }
+
+    public void setHora_entrada(String hora_entrada) {
+        this.hora_entrada = hora_entrada;
+    }
+
+    public String getHora_salida() {
+        return hora_salida;
+    }
+
+    public void setHora_salida(String hora_salida) {
+        this.hora_salida = hora_salida;
+    }
+
+    public long getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(long sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + '}';
+    }
+    
+}
