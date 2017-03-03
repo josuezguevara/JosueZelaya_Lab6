8bb05990e4b294799c96eb5e59858050022df66e
Josue Zelaya <josuezelaya99@hotmail.es>
2017-03-03 15:00:07 -0600

    Clase Articulo

---
+++ JosueZelaya_Lab6/src/josuezelaya_lab6/Articulo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package josuezelaya_lab6;
+
+/**
+ *
+ * @author usuario
+ */
+public class Articulo {
+    private int precio;
+
+    public Articulo() {
+    }
+
+    public Articulo(int precio) {
+        this.precio = precio;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    
+}
