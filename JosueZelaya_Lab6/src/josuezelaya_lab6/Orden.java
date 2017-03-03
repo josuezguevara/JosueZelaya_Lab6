f9a416c67c6ea8fbc381f9b8ea7a839deb0b5e62
Josue Zelaya <josuezelaya99@hotmail.es>
2017-03-03 15:19:34 -0600

    Clase Orden

--- JosueZelaya_Lab6/src/josuezelaya_lab6/Orden.java
+++ JosueZelaya_Lab6/src/josuezelaya_lab6/Orden.java
@@ -10,5 +10,31 @@ package josuezelaya_lab6;
  * @author usuario
  */
 class Orden {
-    
+  private   int cantidad_articulos;
+  private Empleado empleado;
+
+    public Orden() {
+    }
+
+    public Orden(int cantidad_articulos, Empleado empleado) {
+        this.cantidad_articulos = cantidad_articulos;
+        this.empleado = empleado;
+    }
+
+    public int getCantidad_articulos() {
+        return cantidad_articulos;
+    }
+
+    public void setCantidad_articulos(int cantidad_articulos) {
+        this.cantidad_articulos = cantidad_articulos;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+  
 }
